e326194dab2c371acb8b255455df0ee3f1e47c7d
Ruben <ruben1234bac@gmail.com>
2025-02-15 10:17:01 -0600

    chore: Added dialyzer config
    Added dialyxir dep and added step in elixir.yml

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Supermarket.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.18", only: :test}
+      {:excoveralls, "~> 0.18", only: :test},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
     ]
   end
 
