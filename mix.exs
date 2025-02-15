8073514def7ca8071a55acbc812200f4a8e715f3
Ruben <ruben1234bac@gmail.com>
2025-02-15 17:52:23 -0600

    chore: Added digger dep
    Added dep digger to help to work with atoms in next steps

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Supermarket.MixProject do
       {:bandit, "~> 1.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
-      {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:digger, "~> 3.0"}
     ]
   end
 
